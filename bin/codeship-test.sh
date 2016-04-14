50dc71974ea8e8b58017066d31c7b9929a932955
Zeh Ferrari <e@zeh.me>
2016-04-14 16:37:13 +1000

    codeship unit testing

--- bin/codeship-test.sh
+++ bin/codeship-test.sh
@@ -2,7 +2,7 @@
 
 echo "Testing branch: ${CI_BRANCH} with pipe ${PIPE_NUM}"
 
-if [ ${CI_BRANCH} != "GH_PAGES" ] && [ ${PIPE_NUM} == 1 ]; then
+if [ ${CI_BRANCH} != "GH_PAGES" ]; then
     # Run local tests
     echo "Installing global"
     npm install -g bower web-component-tester
@@ -12,6 +12,4 @@ if [ ${CI_BRANCH} != "GH_PAGES" ] && [ ${PIPE_NUM} == 1 ]; then
 
     echo "Starting local WCT tests"
     wct
-else
-    echo "Pipe not used."
 fi
