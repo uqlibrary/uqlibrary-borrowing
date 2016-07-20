8aa4f3f4cd88089accfd046b2ed52033f53e1e90
Lea de Groot <l.degroot@library.uq.edu.au>
2016-07-20 11:57:28 +1000

    refactoring with Jan [PT #117511571]

--- elements/uqlibrary-borrowing-list.js
+++ elements/uqlibrary-borrowing-list.js
@@ -51,72 +51,55 @@
      * If the 'data' property has changed, recalculate the total in fees and inject the patrons id in each sub component
      */
     itemsChanged: function (_, changeValue) {
-      var items = this.items;
+      var items;
       var processed = [];
+      var haveDivider = false;
+
       if (this.sortByDate) {
-        items.sort(function (a, b) {
-          var aDate = new Date(a.date).getTime();
-          var bDate = new Date(b.date).getTime();
-          if (aDate > bDate) {
+        this.items.sort(function (a, b) {
+          var aDate = moment(a.date);
+          var bDate = moment(b.date);
+          if (aDate.isAfter(bDate)) {
             return 1;
-          }
-          if (aDate < bDate) {
+          } else {
             return -1;
           }
-          return 0;
         });
       }
-      var haveDivider = false;
-      for (var i = 0; i < items.length; i++) {
-        items[i].date = new Date(items[i].date);
 
-        var numMinutesLeft = moment(items[i].date).fromNow(true);
+      for (var i = 0; i < this.items.length; i++) {
+        var item = JSON.parse(JSON.stringify(this.items[i]));
+        item.date = new Date(item.date);
+
+        var numMinutesLeft = moment(item.date).fromNow(true);
         var textCheck = 'minutes';
         if (-1 != numMinutesLeft.indexOf(textCheck)) { // different display for items due in an hour or less
-          if (!items[i].hasOwnProperty('dayPrefixText')) {
-            items[i].dayPrefixText = 'Due in';
-          }
-          if (!items[i].hasOwnProperty('day')) {
-            items[i].day = numMinutesLeft.replace(textCheck, '').trim();
-          }
-          if (!items[i].hasOwnProperty('daySuffixText')) {
-            items[i].daySuffixText = textCheck;
-          }
+            item.dayPrefixText = 'Due in';
+            item.day = numMinutesLeft.replace(textCheck, '').trim();
+            item.daySuffixText = textCheck;
 
         } else
         {
-          if (!items[i].hasOwnProperty('day')) {
-            items[i].day = items[i].date.getDate();
-          }
-          if (!items[i].hasOwnProperty('dayPrefixText')) {
-            items[i].dayPrefixText = moment(items[i].date).format('ddd');
-          }
-          if (!items[i].hasOwnProperty('daySuffixText')) {
-            items[i].daySuffixText = moment(items[i].date).format('MMM');
-          }
+            item.day = item.date.getDate();
+            item.dayPrefixText = moment(item.date).format('ddd');
+            item.daySuffixText = moment(item.date).format('MMM');
         }
 
-        if (items[i].class == '' && !items[i].hasOwnProperty('thetime')) {
-          items[i].thetime = moment(items[i].date).format('HH:mm');
+        if (item.class == '') {
+          item.thetime = moment(item.date).format('HH:mm');
         }
 
-        items[i].class += ' item-item';
-/*
-// currently showing a due date on every loan, so deactivate this code
-        if (!this.showEachDate && i > 0 && items[i].date.getDay() === items[i - 1].date.getDay() && items[i].date.getDate() === items[i - 1].date.getDate()) {
-          items[i].day = '';
-          items[i].dayPrefixText = '';
-          items[i].daySuffixText = '';
-          items[i].thetime = '';
-        }
-*/
+        item.class += ' item-item';
+
         //insert divider between past and upcoming
-        if (i > 0 && !haveDivider && new Date().getTime() < items[i].date.getTime() && new Date().getTime() >= items[i - 1].date.getTime()) {
-          items[i - 1].class += ' last';
+        if (i > 0 && !haveDivider 
+          && new Date().getTime() < item.date.getTime() 
+          && new Date().getTime() >= processed[i - 1].date.getTime()) {
+          processed[i - 1].class += ' last';
           haveDivider = true;
           processed.push({ isDivider: true });
         }
-        processed.push(items[i]);
+        processed.push(item);
       }
       if (processed.length > 0) {
         processed[0].class += ' first';
